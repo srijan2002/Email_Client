4ac997130868f666fb205acd0c2e8582a8f0e7f2
Shru-Singh <gshrutisingh@gmail.com>
2021-09-23 18:00:56 +0530

    UI updated

---
+++ StringConstants.example.sql
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  shruti
+ * Created: 23-Sep-2021
+ */
+public class StringConstants {
+     
+    
+    public static final String DB_URL =//Put your mysql database URL here.
+    public static final String USER =  // Put your mysql username here.
+    public static final String PASS = // Put your mysql password here.
+    
+    
+}
